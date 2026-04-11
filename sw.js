e3696469b94e71e7f8198a8ea6c0b09390f269bb
aaronakinocho <158816225+aaronakinocho@users.noreply.github.com>
2026-04-11 18:21:46 +0000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'shrapnel-v10';
+const CACHE = 'shrapnel-v11';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
